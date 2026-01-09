6ce2d0eec34dda25b5df43077c132759542dde35
vedant-lad-247 <vedantlad@yahoo.com>
2026-01-09 13:19:30 +0530

    vedant

--- contact.php
+++ contact.php
@@ -9,7 +9,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Contact Us - City Care Hospital</title>
     
-    <!-- Main CSS -->
+    
     <link rel="stylesheet" href="assets/css/style.css">
     <link rel="stylesheet" href="assets/css/contact.css">
 
